d6a2a39497e78e314d2615c0632573028b99cc3f
Verano <kamoteka143din@gmail.com>
2022-04-16 22:14:09 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+
+const submit = document.querySelector('.submit');
+const container = document.querySelector('.container');
+const thankYou = document.querySelector('.thank-you');
+// const btn1 = document.querySelector('.btn-1');
+// const btn2 = document.querySelector('.btn-2');
+// const btn3 = document.querySelector('.btn-3');
+// const btn4 = document.querySelector('.btn-4');
+// const btn5 = document.querySelector('.btn-5');
+// const btn = document.querySelectorAll('.btn');
+// console.log(btn);
+
+// btn.addEventListener('mouseover', () => {
+
+// })
+
+submit.addEventListener('click', () => {
+    container.style.display = 'none';
+    thankYou.style.display = 'block';
+    console.log('dave i love you');
+
+})
