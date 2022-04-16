17df1eafaba8b78e3cfee76e87116a83c43de32c
Verano <kamoteka143din@gmail.com>
2022-04-16 22:19:06 +0800

    1st commit

--- main.js
+++ main.js
@@ -4,10 +4,7 @@ const container = document.querySelector('.container');
 const thankYou = document.querySelector('.thank-you');
 // const btn1 = document.querySelector('.btn-1');
 // const btn2 = document.querySelector('.btn-2');
-// const btn3 = document.querySelector('.btn-3');
-// const btn4 = document.querySelector('.btn-4');
-// const btn5 = document.querySelector('.btn-5');
-// const btn = document.querySelectorAll('.btn');
+
 // console.log(btn);
 
 // btn.addEventListener('mouseover', () => {
